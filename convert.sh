bab3b7997a5a1754e33b326f86e49064a33c6c85
YoDobchev <yo.dobchev@gmail.com>
2021-09-16 23:17:12 +0300

    git add .faster pakistan technologygit add .

--- convert.sh
+++ convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf $(pwd)/files/*
 wget -O $(pwd)/files/$1.$2 "https://i.4cdn.org/$3/$1.$2"
-ffmpeg -y -i $(pwd)/files/$1.$2 $(pwd)/files/$1M.mp4
+ffmpeg -y -i $(pwd)/files/$1.$2 -crf 22 -preset ultrafast -movflags +faststart  $(pwd)/files/$1M.mp4
